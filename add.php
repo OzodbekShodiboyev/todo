e997f0333d6fcfa458f4bd7147ca1da91271154c
shodiboy_o <ozodbekonline1@gmail.com>
2024-12-14 17:37:17 +0500

    Errors have been corrected

--- add.php
+++ add.php
@@ -1,10 +1,18 @@
 <?php
 require 'config/DB.php';
- 
-if(isset($_POST['submit']) && isset($_POST['todo'])){
-    $todo = $_POST['todo'];
-    echo $todo;
-    
-}
 
-?>
+$db = (new DB())->connect();
+// echo $db;
+if (isset($_POST['todo'])) {
+    $todo = htmlspecialchars(trim($_POST['todo']));
+    $current_date = date('Y-m-d H:i:s');
+    $sql = "INSERT INTO `todos` (`name`, `created_at`, `updated_at`) 
+                VALUES (:todo, :created_at, :updated_at)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':todo', $todo);
+    $stmt->bindParam(':created_at', $current_date);
+    $stmt->bindParam(':updated_at', $current_date);
+    $stmt->execute();
+
+    header('Location: index.php');
+}
--- config/DB.php
+++ config/DB.php
@@ -2,26 +2,27 @@
 class DB
 {
     public $servername = "localhost";
-
     public $dbname = "todo";
     public $username = "root";
     public $password = "";
 
-    public $conn;
+    public $conn; 
 
     public function connect() {
-        $conn = null;
-        
+        $this->conn = null;
+
         try {
-           $this->$conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
-            $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
+            $this->conn = new PDO(
+                "mysql:host=$this->servername;dbname=$this->dbname", 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully<br>";
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
-        return $conn;
-        
+
+        return $this->conn; 
     }
 }
-
-
